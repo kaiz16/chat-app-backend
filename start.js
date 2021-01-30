1ba9fb07b095c724cf59ebbbc9392927bcfc756b
kaiz16 <realkaiz7@gmail.com>
2021-01-30 16:50:13 +0800

    added new stuff

--- start.js
+++ start.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express()
+// REST API ()
+app.use(express.json())
+let chats = [
+    {
+        msg: 'Hi',
+        created_at: new Date
+    },
+    {
+        msg: 'Hello',
+        created_at: new Date
+    }
+]
+app.get('/messages', (req, res) => {
+    // 200
+    res.status(200).send(chats)
+})
 
+app.post('/newMessage', (req, res) => {
+    // 200
+    let message = {
+        msg: req.body.msg,
+        created_at: new Date
+    }
 
+    chats.push(message)
+    res.status(200).send(message)
+})
+
+// will start the app and run the server until you tell it stop
 app.listen(3000, () => {
     console.log('🚀 App is listening on port 3000 🚀')
 })
